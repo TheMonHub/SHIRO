424172756824dcec168e8b256d818b1840915b9b
TheMonHub <mono15.parnthakote@gmail.com>
2025-08-15 23:56:34 +0700

    Hello, EFI!

--- src/bootloader/bootloader.asm
+++ src/bootloader/bootloader.asm
@@ -1,10 +1,16 @@
-section .bss
-    
 section .data
-    
+
+section .rodata
+    hello_world dw 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!', 0xD, 0xA, 0
+section .bss
+
 section .text
-    global main
-
-main:
-
-    
+    global _start
+_start:
+    mov rcx, [rdx + 64]
+    mov rax, [rcx + 8]
+    mov rdx, hello_world
+    sub rsp, 32
+    call rax
+    add rsp, 32
+    ret
