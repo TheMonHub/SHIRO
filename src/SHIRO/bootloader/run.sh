5117289ac93ac876f84f5c2f77cd14f951bc7451
TheMonHub <mono15.parnthakote@gmail.com>
2025-08-23 23:07:36 +0700

    License!!

--- src/SHIRO/bootloader/run.sh
+++ src/SHIRO/bootloader/run.sh
@@ -1,2 +1,20 @@
+#
+# SHIRO project, an operating system, kernel and bootloader.
+# Copyright (C) 2025 TheMonHub
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 sh -c "[ -f OVMF_VARS.4m.fd ] || cp /usr/share/edk2/x64/OVMF_VARS.4m.fd ./"
 qemu-system-x86_64 -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd -drive if=pflash,format=raw,file=./OVMF_VARS.4m.fd -drive format=raw,file=fat:rw:boot/
