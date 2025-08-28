2d4310abf3e14fb803259fa666342bad37789863
TheMonHub <mono15.parnthakote@gmail.com>
2025-08-28 20:58:58 +0700

    config the clang tidy

--- clang-tidy-fix.sh
+++ clang-tidy-fix.sh
@@ -17,7 +17,7 @@
 #
 
 # shellcheck disable=SC2046
-cmake -S . -B build -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DLUMI_BUILD_TESTS=ON -DLUMI_BUILD_EXAMPLES=ON -DLUMI_BUILD_ALL=ON -DLUMI_BUILD_DOCS=OFF -D CMAKE_C_COMPILER=$(which clang) -D CMAKE_CXX_COMPILER=$(which clang++) -G Ninja
+cmake -S . -B build -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -G Ninja
 cmake --build build
 # shellcheck disable=SC2046
 run-clang-tidy '^(?!.*(\.asm|\.inc|_deps/))' -p build -quiet
